6d8e07408eb185b526b6abd6f2dc20cb737a1a39
dishuiGit <dishui_git@126.com>
2017-04-13 16:21:27 +0800

    2017-04-13

---
+++ post/bigdata/spark/tmp.scala
@@ -0,0 +1,52 @@
+(php.itcast.cn,http://php.itcast.cn/php/course.shtml,459),
+(java.itcast.cn,http://java.itcast.cn/java/course/base.shtml,543),
+(java.itcast.cn,http://java.itcast.cn/java/video.shtml,496),
+(java.itcast.cn,http://java.itcast.cn/java/course/android.shtml,501),
+(net.itcast.cn,http://net.itcast.cn/net/video.shtml,521),
+(java.itcast.cn,http://java.itcast.cn/java/course/hadoop.shtml,506),
+(net.itcast.cn,http://net.itcast.cn/net/course.shtml,521),
+(java.itcast.cn,http://java.itcast.cn/java/course/cloud.shtml,1028),
+(php.itcast.cn,http://php.itcast.cn/php/video.shtml,490),
+(java.itcast.cn,http://java.itcast.cn/java/teacher.shtml,482),
+(php.itcast.cn,http://php.itcast.cn/php/teacher.shtml,464),
+(net.itcast.cn,http://net.itcast.cn/net/teacher.shtml,512),
+(java.itcast.cn,http://java.itcast.cn/java/course/javaee.shtml,1000),
+(java.itcast.cn,http://java.itcast.cn/java/course/javaeeadvanced.shtml,477)
+
+
+
+
+
+(net.itcast.cn,CompactBuffer(
+    (net.itcast.cn,http://net.itcast.cn/net/video.shtml,521),
+    (net.itcast.cn,http://net.itcast.cn/net/course.shtml,521),
+    (net.itcast.cn,http://net.itcast.cn/net/teacher.shtml,512))),
+(java.itcast.cn,CompactBuffer(
+    (java.itcast.cn,http://java.itcast.cn/java/course/base.shtml,543),
+    (java.itcast.cn,http://java.itcast.cn/java/video.shtml,496),
+    (java.itcast.cn,http://java.itcast.cn/java/course/android.shtml,501),
+    (java.itcast.cn,http://java.itcast.cn/java/course/hadoop.shtml,506),
+    (java.itcast.cn,http://java.itcast.cn/java/course/cloud.shtml,1028),
+    (java.itcast.cn,http://java.itcast.cn/java/teacher.shtml,482),
+    (java.itcast.cn,http://java.itcast.cn/java/course/javaee.shtml,1000),
+    (java.itcast.cn,http://java.itcast.cn/java/course/javaeeadvanced.shtml,477))),
+(php.itcast.cn,CompactBuffer(
+    (php.itcast.cn,http://php.itcast.cn/php/course.shtml,459),
+    (php.itcast.cn,http://php.itcast.cn/php/video.shtml,490),
+    (php.itcast.cn,http://php.itcast.cn/php/teacher.shtml,464)))
+
+
+
+
+(net.itcast.cn,List(
+    (net.itcast.cn,http://net.itcast.cn/net/course.shtml,521),
+    (net.itcast.cn,http://net.itcast.cn/net/video.shtml,521),
+    (net.itcast.cn,http://net.itcast.cn/net/teacher.shtml,512))),
+(java.itcast.cn,List(
+    (java.itcast.cn,http://java.itcast.cn/java/course/cloud.shtml,1028),
+    (java.itcast.cn,http://java.itcast.cn/java/course/javaee.shtml,1000),
+    (java.itcast.cn,http://java.itcast.cn/java/course/base.shtml,543))),
+(php.itcast.cn,List(
+    (php.itcast.cn,http://php.itcast.cn/php/video.shtml,490),
+    (php.itcast.cn,http://php.itcast.cn/php/teacher.shtml,464),
+    (php.itcast.cn,http://php.itcast.cn/php/course.shtml,459)))
