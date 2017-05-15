3f2995851a8168ed459a743578a428c6f77b1541
dishuiGit <dishui_git@126.com>
2017-05-15 09:00:38 +0800

    2017-05-15

---
+++ post/bigdata/hadoop/docker/dockerfile/azkaban/t.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cat > /etc/apt/sources.list <<_EOF_
+deb http://mirrors.aliyun.com/debian/ jessie main non-free contrib
+deb http://mirrors.aliyun.com/debian/ jessie-proposed-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ jessie main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ jessie-proposed-updates main non-free contrib
+_EOF_
