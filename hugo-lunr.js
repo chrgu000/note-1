9430baf7be5b9f8ad78e0c22fa6930695fd73f3d
dishuiGit <dishui_git@126.com>
2017-06-05 17:46:38 +0800

    2017-06-05

--- hugo-lunr.js
+++ hugo-lunr.js
@@ -1,5 +1,5 @@
 var hugolunr = require('hugo-lunr');
 var h = new hugolunr();
-h.setInput('/hugo/content/post/**');
+h.setInput('post/**');
 h.setOutput('/hugo/themes/mainroad/static/js/example_data.json');
 h.index();
